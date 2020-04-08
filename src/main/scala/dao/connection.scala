4f5cad971407f605b2b2a70cc46aac0097d1a34e
Jade Shan <evokeralucard@gmail.com>
2020-04-08 11:06:58 +0800

    Hikari JDBC POOL

--- src/main/scala/dao/connection.scala
+++ src/main/scala/dao/connection.scala
@@ -8,10 +8,15 @@ import com.zaxxer.hikari.util.DriverDataSource;
 
 import jadeutils.common.Logging
 import java.sql.Connection
+import scala.util.Try
+import scala.util.Success
+import java.sql.SQLException
+import org.scalactic.Fail
+import scala.util.Failure
 
 trait DatabaseConnectionPool {
 
-	def getConnection(): Connection
+	def borrow(): Try[Connection]
 }
 
 
@@ -20,6 +25,17 @@ class HikariConnectionPool(val props: Properties) extends DatabaseConnectionPool
 	val cfg = new HikariConfig(props);
 	val ds = new HikariDataSource(cfg);
 
-	def getConnection() = ds.getConnection()
+	def borrow(): Try[Connection] = try {
+		Success(ds.getConnection())
+	} catch {
+		case e: SQLException => Failure(e)
+	}
+	
+	def retrunBack(connection: Try[Connection]): Unit = connection match {
+		case Success(conn) => conn.close()
+		case Failure(e) => { /* do nothing */}
+	}
+		
+	
 }
 
