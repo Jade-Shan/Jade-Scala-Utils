25add040ab6e29bb2fff978b11e436aef1c758f5
Jade Shan <evokeralucard@gmail.com>
2020-04-16 14:20:09 +0800

    mysql dao test

---
+++ src/test/scala/dao/mysqlDaoTest.scala
@@ -0,0 +1,90 @@
+package jadeutils.comm.dao
+
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.FunSuite
+import org.junit.runner.RunWith
+
+import jadeutils.common.Logging
+import jadeutils.common.EnvPropsComponent
+
+import java.sql.DriverManager
+import java.sql.Connection
+import java.util.Properties
+import scala.util.Try
+import scala.util.Failure
+import scala.util.Success
+import java.util.Date
+
+class MysqlImplTestDao(dataSource: DataSourcetHolder) //
+extends JDBCTemplateDao[User, String](dataSource) with Logging {
+	
+}
+
+@RunWith(classOf[JUnitRunner])
+class MysqlDaoImplTest extends FunSuite with Logging {
+	val dao = new MysqlImplTestDao(MysqlDataSourceHolder)
+
+	test("Test-dao-impl-test-00-sql-inset-get") {
+		MysqlEnv.testInEnv(() => {
+			MysqlDataSourceHolder.connection().get.setAutoCommit(true)
+			val now = new Date(System.currentTimeMillis())
+			val user = new User("1", "Jade", now, now)
+			dao.insert(user)
+			logDebug("------------after insert")
+			val u = dao.getById("1")
+			logDebug("------------after load: {}", u)
+			assert(u.isSuccess)
+			assert(u.get.id == "1")
+			assert(u.get.name == "Jade")
+			logDebug("all : {}", dao.query("select * from testuser where 1=1"))
+		})
+	}
+
+	test("Test-dao-impl-test-01-sql-inset-update") {
+		MysqlEnv.testInEnv(() => {
+			MysqlDataSourceHolder.connection().get.setAutoCommit(true)
+			val now = new Date(System.currentTimeMillis())
+			val user = new User("1", "Jade", now, now)
+			dao.insert(user)
+			logDebug("------------after insert")
+			val u = dao.getById("1")
+			logDebug("------------after load: {}", u)
+			assert(u.isSuccess)
+			assert(u.get.id == "1")
+			assert(u.get.name == "Jade")
+
+			user.name = "Jade Update"
+			dao.update(user)
+			logDebug("------------after update")
+			val v = dao.getById("1")
+			logDebug("------------after load: {}", v)
+			assert(v.isSuccess)
+			assert(v.get.id == "1")
+			assert(v.get.name == "Jade Update")
+		})
+	}
+
+	test("Test-dao-impl-test-02-sql-inset-or-update") {
+		MysqlEnv.testInEnv(() => {
+			MysqlDataSourceHolder.connection().get.setAutoCommit(true)
+			val now = new Date(System.currentTimeMillis())
+			val user = new User("1", "Jade", now, now)
+			dao.insert(user)
+			logDebug("------------after insert")
+			val u = dao.getById("1")
+			logDebug("------------after load: {}", u)
+			assert(u.isSuccess)
+			assert(u.get.id == "1")
+			assert(u.get.name == "Jade")
+
+			user.name = "Jade Update"
+			dao.insertOrUpdate(user)
+			logDebug("------------after update")
+			val v = dao.getById("1")
+			logDebug("------------after load: {}", v)
+			assert(v.isSuccess)
+			assert(v.get.id == "1")
+			assert(v.get.name == "Jade Update")
+		})
+	}
+}
